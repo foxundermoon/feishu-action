db73b0f52c04d71ae030cd58e79b1e99a19529a7
Ayaka Neko <neko@ayaka.moe>
2022-07-05 12:01:48 +0800

    feat: support feishu interactive card message

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,30 @@ import * as core from '@actions/core'
 import got from 'got'
 import yaml from 'js-yaml'
 
+
+/** Supported push message format https://open.feishu.cn/document/ukTMukTMukTM/ucTM5YjL3ETO24yNxkjN#8b0f2a1b */
+enum MsgType {
+  Text = 'text',
+  Post = 'post',
+  Image = 'image',
+  ShareChat = 'share_chat',
+  Interactive = 'interactive'
+}
+
+/** Message */
 interface Message {
   msg_type: string
   content: any
 }
 
-async function postMessage(): Promise<string> {
-  const msg_type: string = core.getInput('msg_type')
+/** Interactive Message, for interactive msg_type */
+interface InteractiveMessage {
+  msg_type: string
+  card: any
+}
+
+/** Send text/post/image/sharechat message */
+async function postNormalMessage(msg_type: MsgType): Promise<string> {
   const content: string = core.getInput('content')
   return await post({
     msg_type,
@@ -16,7 +33,32 @@ async function postMessage(): Promise<string> {
   })
 }
 
-async function post(body: Message): Promise<string> {
+/** Send interactive message */
+async function postInteractiveMessage(msg_type: MsgType): Promise<string> {
+  const cardContent: string = core.getInput('content')
+  return await post({
+    msg_type,
+    card: yaml.load(cardContent)
+  })
+}
+
+async function postMessage(): Promise<string> {
+  const msg_type = core.getInput('msg_type') as MsgType
+
+  switch (msg_type) {
+    case MsgType.Text:
+    case MsgType.Post:
+    case MsgType.Image:
+    case MsgType.ShareChat:
+      return await postNormalMessage(msg_type)
+    case MsgType.Interactive:
+      return await postInteractiveMessage(msg_type)
+    default: // fallback
+      return await postNormalMessage(msg_type)
+  }
+}
+
+async function post(body: Message | InteractiveMessage): Promise<string> {
   const url: string = core.getInput('url')
   const rsp = await got.post(url, {
     headers: {
@@ -33,7 +75,7 @@ async function run(): Promise<void> {
   try {
     await postMessage()
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed((error as any).message)
   }
 }
 
